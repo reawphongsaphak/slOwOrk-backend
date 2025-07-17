16865f2bfe1e2b59c5e42c1e0368131a17d84787
reawphongsaphak <135101095+reawphongsaphak@users.noreply.github.com>
2025-07-17 23:21:33 +0700

    Refactor

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	
+	"github.com/reawphongsaphak/slOwOrk-Backend/config"
+	"github.com/reawphongsaphak/slOwOrk-Backend/database"
+	"github.com/reawphongsaphak/slOwOrk-Backend/routes"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, World")
+	err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	cfg := config.LoadConfig()
+
+	database.ConnectDB()
+
+	// Initialize Gin
+	r := gin.Default()
+	r.Use(cors.Default())
+	
+	routes.SetupRoutes(r)
+
+	r.Run(cfg.Port)
 }
