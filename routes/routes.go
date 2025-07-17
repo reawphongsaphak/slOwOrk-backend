df8858cfd99d2775801e27cabb413fab384519a1
reawphongsaphak <135101095+reawphongsaphak@users.noreply.github.com>
2025-07-17 23:20:00 +0700

    Add: routes

---
+++ routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/reawphongsaphak/slOwOrk-Backend/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(r *gin.Engine) {
+	api := r.Group("/api/v1") 
+	{
+		api.POST("/register", controllers.RegisterNewUser)
+	}	
+}
