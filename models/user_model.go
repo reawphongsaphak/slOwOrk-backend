7fa033c5e8cbdb1cfd6d2e2f6667774dc75e1214
reawphongsaphak <reawza00789@gmail.com>
2025-07-18 10:53:36 +0700

    Add: login user model

--- models/user_model.go
+++ models/user_model.go
@@ -13,6 +13,11 @@ type User struct {
 	Detail 		Detail	`json:"detail" binding:"required"`
 }
 
+type LoginUser struct {
+	Email		string `json:"email" binding:"required,email"`
+	Password	string `json:"password" binding:"required"`
+}
+
 type Detail struct {
 	Name	string		`json:"name" binding:"required"`
 	Role	[]string	`json:"role" binding:"required"`
