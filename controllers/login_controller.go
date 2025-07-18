4e6b0780e307255b2135f994c491a0a049b613f8
reawphongsaphak <reawza00789@gmail.com>
2025-07-18 10:54:17 +0700

    Edit: login

--- controllers/login_controller.go
+++ controllers/login_controller.go
@@ -1,5 +1,34 @@
 package controllers
 
-func LoginUser() {
-	
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/reawphongsaphak/slOwOrk-Backend/errors"
+	"github.com/reawphongsaphak/slOwOrk-Backend/models"
+	"github.com/reawphongsaphak/slOwOrk-Backend/services"
+)
+
+func LoginUser(c *gin.Context) {
+	var loginUser models.LoginUser
+
+	if err := c.ShouldBindJSON(&loginUser); err != nil {
+		c.JSON(errors.ErrBadRequest.Code, gin.H{
+			"error": errors.ErrBadRequest.Message,
+		})
+		return
+	}
+
+	tokenString, err := services.LoginUser(loginUser)
+	if err != nil {
+		c.JSON(errors.ErrInternalServer.Code, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	//* set time out to 1 hour
+	c.SetCookie("Authorization", tokenString, 60*60, "", "", false, true)
+	c.JSON(http.StatusOK, gin.H{})
 }
